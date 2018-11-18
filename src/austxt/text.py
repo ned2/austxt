f4836bf88c326f291e1bab43192fabd3ddec6372
Ned Letcher <nletcher@gmail.com>
2018-11-18 13:39:25 +1100

    Strip stop words from cleaned text

--- src/austxt/text.py
+++ src/austxt/text.py
@@ -6,4 +6,4 @@ nlp = spacy.load('en_core_web_sm', disable=['parser', 'tagger'])
 
 def clean_text(text):
     return ' '.join(token.lower_ for token in nlp(text)
-                    if not (token.is_punct or token.is_space))
+                    if not (token.is_stop or token.is_punct or token.is_space))
